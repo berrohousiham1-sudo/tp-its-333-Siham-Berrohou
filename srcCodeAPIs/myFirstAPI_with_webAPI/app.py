b2c87b50c429284df31d2e90d7f9cd681b65331d
BERROHOU SIHAM <Berrohousiham1@gmail.com>
2026-01-14 11:27:45 +0100

    Finalisation des exercices my first API 1 et 2

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -3,9 +3,24 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 ## EXO1: API GET: renvoyer un helloworld - API end point name: "api/salutation"
-
+@app.route('/api/salutation', methods=['GET'])
+def hello_world():
+    return jsonify({"message": "Hello, World!"})
 ## EXO2: API POST: renvoyer un nom fourni en parametre - API end point name: "api/utilisateurs"
-
+@app.route('/api/utilisateurs', methods=['POST'])
+def post_nom():
+    # On récupère les données JSON envoyées dans le corps (body) de la requête
+    data = request.get_json()
+    
+    # On extrait la valeur associée à la clé "nom"
+    # .get('nom') permet d'éviter une erreur si la clé est absente
+    nom_utilisateur = data.get('nom', 'Utilisateur inconnu')
+    
+    # On retourne une réponse au format JSON
+    return jsonify({
+        "message": "Nom reçu avec succès",
+        "nom": nom_utilisateur
+    })
 # to be tested with curl
 
 # Commandes bash (attention, à adapter pour powershell)
--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -2,7 +2,16 @@ from app import app
 from flask import render_template, request, jsonify
 
 ### EXO1 - Implement a route that returns "hello" printed in a webpage using an html template
-
+@app.route('/hello')
+def exo1():
+    return render_template('index.html')
 ### EXO2 - Implement a route that returns "hello {name}" with the name declared in the route (add html template)
-
+@app.route('/hello/<name>')
+def exo2(name):
+    return render_template('index.html', name=name)
 ### EXO3 - Implement a route that returns "hello {name}" with the name retrieved from url, eg retrieved from http://localhost:5000/api/salutation?nom=Alice
+@app.route('/hello_query')
+def exo3():
+    # Extrait 'name' depuis les arguments de la requête
+    nom_recupere = request.args.get('name', 'Stranger')
+    return render_template('index.html', name=nom_recupere)
